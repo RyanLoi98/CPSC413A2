a7ff1625f26e680742a3f20c3815ca24d1cb512a
Ryan <ryan.loi@ucalgary.ca>
2024-03-04 05:03:43 -0700

    fixed start and end of left and right array determination.

--- array_checker.py
+++ array_checker.py
@@ -7,10 +7,10 @@ class Checker(object):
         # recursive case when the array has more than one element
         if start != end:
             # recurse on the left side of the array
-            L_max_left_aligned_sum, L_max_right_aligned_sum, L_max_subarray_sum, L_sum_of_all_elements = self.lecture_algorithm(start, (end//2))
+            L_max_left_aligned_sum, L_max_right_aligned_sum, L_max_subarray_sum, L_sum_of_all_elements = self.lecture_algorithm(start, ((start + end)//2))
 
             # recurse on the right side of the array
-            R_max_left_aligned_sum, R_max_right_aligned_sum, R_max_subarray_sum, R_sum_of_all_elements = self.lecture_algorithm(((end//2) + 1), end)
+            R_max_left_aligned_sum, R_max_right_aligned_sum, R_max_subarray_sum, R_sum_of_all_elements = self.lecture_algorithm((((start + end)//2) + 1), end)
 
             # calculate the 4 return values
             max_left_aligned_sum = max(L_max_left_aligned_sum, (L_sum_of_all_elements + R_max_left_aligned_sum))
@@ -28,14 +28,14 @@ class Checker(object):
 
             if self.array[start] < 0:
                 sum_of_all_elements = self.array[start]
-                return max_left_aligned_sum, max_right_aligned_sum, max_subarray_sum, sum_of_all_elements
 
             else:
                 max_left_aligned_sum = self.array[start]
                 max_right_aligned_sum = self.array[start]
                 max_subarray_sum = self.array[start]
                 sum_of_all_elements = self.array[start]
-                return max_left_aligned_sum, max_right_aligned_sum, max_subarray_sum, sum_of_all_elements
+
+            return max_left_aligned_sum, max_right_aligned_sum, max_subarray_sum, sum_of_all_elements
 
     def max_sums(self, start, end):
         # return the maximum possible sum of a subarray with indices between start and end (inclusive)
