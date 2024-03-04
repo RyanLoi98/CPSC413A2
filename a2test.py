92802b97480392536ad7da6d22dc1141de57b98e
Ryan <ryan.loi@ucalgary.ca>
2024-03-04 05:22:36 -0700

    added more test cases

--- a2test.py
+++ a2test.py
@@ -4,4 +4,16 @@ checker = array_checker.Checker([-2, -3, 4, -1, -2, 1, 5, -3])
 print(checker.max_sums(0, 7)) # 7
 print(checker.max_sums(2, 7)) # 7
 print(checker.max_sums(0, 5)) # 4
-print(checker.max_sums(0, 0)) # -2
+print(checker.max_sums(0, 0)) # 0
+print(checker.max_sums(0, 1)) # 0
+print(checker.max_sums(0, 2)) # 4
+print(checker.max_sums(0, 3)) # 4
+print(checker.max_sums(0, 4)) # 4
+print(checker.max_sums(0, 5)) # 4
+print(checker.max_sums(0, 6)) # 7
+print(checker.max_sums(7, 7)) # 0
+print(checker.max_sums(6, 7)) # 5
+print(checker.max_sums(5, 7)) # 6
+print(checker.max_sums(3, 3)) # 0
+print(checker.max_sums(1, 3)) # 4
+print(checker.max_sums(2, 5)) # 4
